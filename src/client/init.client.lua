1ebd4117b8555a37dabb0539deb78574a781cb6c
Влад Кот <vanderofcat@gmail.com>
2022-12-16 18:52:22 +0300

    fusion just looks better (also fixes respawn bug)

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,27 +1,33 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 
-local Roact = require(ReplicatedStorage.Common.Roact)
+local Fusion = require(ReplicatedStorage.Common.Fusion)
+local New = Fusion.New
+local Children = Fusion.Children
 
-local Time = game.ReplicatedStorage.TimerValue
-local Text = game.ReplicatedStorage.TimerText
+local State = Fusion.State
+local Computed = Fusion.Computed
 
--- Create a function that creates the elements for our UI.
--- Later, we'll use components, which are the best way to organize UI in Roact.
-local function clock(currentTime, text)
-    return Roact.createElement("ScreenGui", {}, {
-        TimeLabel = Roact.createElement("TextLabel", {
-            Size = UDim2.new(.2, 0, .1, 0),
-            Text = text .. currentTime
-        })
-    })
-end
+local Time = State(game.ReplicatedStorage.TimerValue.Value)
+local Text = State(game.ReplicatedStorage.TimerText.Value)
 
-local PlayerGui = Players.LocalPlayer.PlayerGui
-local handle = Roact.mount(clock(Time.Value, Text.Value), PlayerGui, "Clock UI")
--- Every second, update the UI to show our new time.
+local Clock = Computed(function()
+    return Text:get() .. Time:get()
+end)
+
+local gui = New "ScreenGui" {
+    Parent = Players.LocalPlayer.PlayerGui,
+    Name = "Clock",
+    [Children] = New "TextLabel" {
+        Size = UDim2.new(.2, 0, .1, 0),
+        Text = Clock
+    }
+}
+
+-- Every second, update the state
 while true do
     wait(1)
-    handle = Roact.update(handle, clock(Time.Value, Text.Value))
+    Time:set(game.ReplicatedStorage.TimerValue.Value)
+    Text:set(game.ReplicatedStorage.TimerText.Value)
 end
 
