65b52c18de54c1eaf554e1f55273964b9fd0e4c3
Влад Кот <vanderofcat@gmail.com>
2022-12-19 12:46:42 +0300

    fix end

--- src/server/mazeComplete.server.lua
+++ src/server/mazeComplete.server.lua
@@ -1,4 +1,4 @@
---workspace.TheEnd:MoveTo(Vector3.new(419.925, -53.015, 574.5))
+workspace.TheEnd:MoveTo(Vector3.new(419.925, -53.015, 574.5))
 local Players = game:GetService("Players")
 local RemoteStorage = game:GetService("ReplicatedStorage")
 workspace.TheEnd.IntroWalls.EmancipationGrill.EmancipationGrillMaterial.kill_Brick.Touched:Connect(function(otherPart)
